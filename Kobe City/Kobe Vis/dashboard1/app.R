46a82d41740fd47a61e855e63c29a4e769ebaef8
Kento Shigyo <kentoglwo@gmail.com>
2023-10-21 23:51:07 +0800

    :sparkles: interactive map and bar chart of population

--- Kobe City/Kobe Vis/dashboard1/app.R
+++ Kobe City/Kobe Vis/dashboard1/app.R
@@ -22,14 +22,40 @@ ui <- navbarPage("Kobe Vis",
                                   "西区"),
                    selected = "全市",
                    multiple = FALSE
-      )
+      ),
+      sliderInput("year", "Year:",
+                  min = 2014, max = 2023,
+                  value = 2023, step = 1,
+                  sep="",
+                  animate =
+                    animationOptions(interval = 1, loop = TRUE)
+      ),
+      sliderInput("month", "Month:",
+                  min = 1, max = 9,
+                  value = 1, step = 1,
+                  sep="",
+                  animate =
+                    animationOptions(interval = 3000, loop = TRUE)
+      ),
+      checkboxGroupInput("checkbox_people", "Attribute of people:",
+                         c("Japanese" = "japanese",
+                           "Foreiners" = "foreigners"),
+                         selected=c("japanese", "foreigners")
+      ),
+      checkboxGroupInput("checkbox_gender", "Gender:",
+                         c("Male" = "male", "Female" = "female"),
+                          selected=c("male", "female")
+      ),
      ),
+    
      mainPanel(
-       plotOutput(outputId = "barPopulation"),
+       # plotOutput(outputId = "barPopulation"),
        # plotOutput(outputId = "map_all_wards"),
-       plotOutput(outputId = "map_population"),
-       plotOutput(outputId = "map_population_density"),
-       plotOutput(outputId = "map_location_city_ward_offices")
+       # plotOutput(outputId = "map_population"),
+       # plotOutput(outputId = "map_population_density"),
+       # plotOutput(outputId = "map_location_city_ward_offices")
+       plotOutput(outputId = "map_population_by_year_month"),
+       plotOutput(outputId =  "plot_bar_population_year_month")
      )
    )
   ),
@@ -52,17 +78,26 @@ server <- function(input, output) {
   output$map_all_wards <- renderPlot({
    map_all_wards(input$target_areas)
   })
-  
+
   output$map_population <- renderPlot({
     map_population(input$target_areas)
   })
-  
+
   output$map_population_density <- renderPlot({
     map_population_density(input$target_areas)
   })
-  
+
   output$map_location_city_ward_offices <- renderPlot({
     map_location_city_ward_offices(input$target_areas)
+  })
+  
+  output$plot_bar_population_year_month <- renderPlot({
+    plot_bar_population_year_month(input$target_areas, input$year, input$month)
+  })
+  
+  
+  output$map_population_by_year_month <- renderPlot({
+    map_population_by_year_month(input$target_areas, input$year, input$month)
   })
 }
 
--- Kobe City/Kobe Vis/dashboard1/helpers.R
+++ Kobe City/Kobe Vis/dashboard1/helpers.R
@@ -9,6 +9,7 @@ library(RColorBrewer)
 
 d_daicho <- read_csv("../data/2023-juminkihondaicho.csv")
 
+###################
 d_daicho_1 <- d_daicho %>%
   filter(種別 == '人口(計)') %>%
   filter(対象年月 == 202301) %>%
@@ -21,6 +22,8 @@ d_daicho_2 <- d_daicho %>%
   filter(区 == '北区' | 区 == '須磨区' | 区 == '西区') %>%
   group_by(区) %>% filter(値 == max(値))
 d_daicho_districts <- union(d_daicho_1, d_daicho_2)
+###################
+
 
 plot_bar_population <- function(target_area){
   d_daicho_districts %>%
@@ -42,6 +45,8 @@ map2 <- map %>%
   mutate(CITY_NAME = str_sub(CITY_NAME, 4,-1)) %>%
   left_join(d_daicho_districts2, by="CITY_NAME") %>% 
   select(CITY_NAME, AREA, 値) %>% rename(population=値)
+
+
 
 map_all_wards <-function(target_area){
   head(map2)
@@ -122,3 +127,99 @@ map_location_city_ward_offices <- function(target_area){
       scale_color_discrete(breaks=c("市役所", "区役所", "支所")) +
       ggtitle("神戸市内の市役所・区役所・支所の位置")
 }
+
+
+######################
+d_daicho_population1 <- d_daicho %>% 
+  filter(区 != '全市') %>%
+  filter(支所等 == '本区' | 支所等 == '合計') %>%
+  filter(区 != '北区' & 区 != '須磨区' & 区 != '西区')
+
+
+d_daicho_population2 <- d_daicho %>% 
+  filter(区 != '全市') %>%
+  filter(支所等 == '本区' | 支所等 == '合計') %>%
+  filter(区 == '北区' | 区 == '須磨区' | 区 == '西区') %>% 
+  group_by(区, 対象年月) %>% filter(値 == max(値)) 
+
+d_daicho_population <- union(d_daicho_population1, d_daicho_population2) %>% 
+  select(対象年月, 区, 種別, 値)
+
+
+map <- read_sf("../data/Shape Files/kobe_districts/all_districts/all_districts.shp")
+d_daicho_districts2 <- d_daicho_districts %>% rename(CITY_NAME=区)
+map2 <- map %>% 
+  mutate(CITY_NAME = str_sub(CITY_NAME, 4,-1)) %>%
+  left_join(d_daicho_districts2, by="CITY_NAME") %>% 
+  select(CITY_NAME, AREA, 値) %>% rename(population=値)
+
+
+d_daicho_population2 <- d_daicho_population %>% rename(CITY_NAME=区)
+map3 <- map  %>% 
+  select(CITY_NAME, AREA) %>% 
+  mutate(CITY_NAME = str_sub(CITY_NAME, 4,-1)) %>%
+  full_join(d_daicho_population2, by="CITY_NAME") %>% 
+  rename(population=値)
+
+
+plot_bar_population_year_month <- function(target_area, year, month){
+  target_month_str <- if_else(month < 10, str_glue("0{month}"), as.character(month))
+  target_year_month_str <- as.double(str_glue("{year}{target_month_str}"))
+  
+  d_daicho_population2 %>%
+    filter(種別 == "人口(計)") %>% 
+    filter(対象年月 == target_year_month_str) %>%
+    filter(if(target_area == "全市") TRUE else CITY_NAME ==  target_area) %>%
+    ggplot() +
+    geom_bar(aes(x=reorder(CITY_NAME, -値), y=値), stat="identity", fill="steelblue") +
+    geom_text(aes(x=CITY_NAME, y=値, label=scales::comma(値)), vjust=-0.3) +
+    ggtitle("神戸市の区ごとの人口数") +
+    xlab("区") +
+    ylab("人数") +
+    scale_y_continuous(labels=comma)
+}
+
+map_population_by_year_month <- function(target_area, year=2023, month=1){
+  target_month_str <- if_else(month < 10, str_glue("0{month}"), as.character(month))
+  target_year_month_str <- as.double(str_glue("{year}{target_month_str}"))
+  
+  map3 %>% 
+    filter(if(target_area == "全市") TRUE else CITY_NAME ==  target_area) %>%
+    filter(種別 == "人口(計)") %>% 
+    filter(対象年月 == target_year_month_str) %>%
+    ggplot() +
+      geom_sf(aes(fill=population)) +
+      scale_fill_gradientn(colors=brewer.pal(11,"GnBu"), labels=comma) +
+      theme_void() +
+      geom_text(aes(x = 135.18, y = 34.8), label = "北区", color="white", family = "HiraKakuProN-W3") +
+      geom_text(aes(x = 135.04, y = 34.72), label = "西区", color="white", family = "HiraKakuProN-W3") +
+      geom_text(aes(x = 135.06, y = 34.65), label = "垂水区", color="white", family = "HiraKakuProN-W3") +
+      geom_text(aes(x = 135.108, y = 34.67), label = "須磨区", color="black", family = "HiraKakuProN-W3") +
+      geom_text(aes(x = 135.215, y = 34.67), label = "中央区", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.227, y = 34.72), label = "灘区", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.27, y = 34.72), label = "東灘区", color="white", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.145, y = 34.67), label = "長", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.147, y = 34.658), label = "田", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.149, y = 34.646), label = "区", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.165, y = 34.68), label = "兵", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.170, y = 34.665), label = "庫", color="black", family = "HiraKakuProN-W3", angle=0) +
+      geom_text(aes(x = 135.175, y = 34.65), label = "区", color="black", family = "HiraKakuProN-W3", angle=0) +
+      labs(fill = "単位：人") +
+      ggtitle("神戸市各区の人口")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
